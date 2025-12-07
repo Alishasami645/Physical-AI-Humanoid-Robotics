0dfbf9529b919da689a5374536f1bad902ab4d09
unknown <alishasami804@gmail.com>
2025-12-07 19:41:49 +0500

    first commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,11 +1,74 @@
+// module.exports = {
+//   title: "Physical AI & Humanoid Robotics",
+//   url: "https://github.com/Alishasami645",
+//   baseUrl: "/Physical-AI-Humanoid-Robotics",
+//   onBrokenLinks: "throw",
+//   favicon: "img/favicon.ico",
+
+//   organizationName: "alishasami645",
+//   projectName: "Physical-AI-Humanoid-Robotics",
+
+//   presets: [
+//     [
+//       "@docusaurus/preset-classic",
+//       {
+//         docs: {
+//           sidebarPath: require.resolve("./sidebars.js"),
+//           editUrl:
+//             "https://github.com/Alishasami645/Physical-AI-Humanoid-Robotics/tree/main/",
+//         },
+//         theme: {
+//           customCss: require.resolve("./src/css/custom.css"),
+//         },
+//       },
+//     ],
+//   ],
+
+//   markdown: {
+//     hooks: {
+//       onBrokenMarkdownLinks: "warn",
+//     },
+//   },
+
+//   themeConfig: {
+//     navbar: {
+//       title: "Physical AI & Humanoid Robotics",
+//       logo: { alt: "Logo", src: "img/logo-book.png" },
+//       items: [
+//         {
+//           type: "docSidebar",
+//           sidebarId: "tutorialSidebar",
+//           position: "left",
+//           label: "Book",
+//         },
+//         {
+//           href: "https://github.com/Alishasami645/Physical-AI-Humanoid-Robotics",
+//           label: "GitHub",
+//           position: "right",
+//         },
+//       ],
+//     },
+//   },
+// };
+
+
+
+
+
+
+
+
 module.exports = {
   title: "Physical AI & Humanoid Robotics",
-  url: "https://github.com/Alishasami645",
-  baseUrl: "/Physical-AI-Humanoid-Robotics",
-  onBrokenLinks: "throw",
-  favicon: "img/favicon.ico",
 
-  organizationName: "alishasami645",
+  // ✅ main site URL (GitHub Pages root)
+  url: "https://alishasami645.github.io",
+
+  // ✅ repo folder
+  baseUrl: "/Physical-AI-Humanoid-Robotics/",
+
+  favicon: "img/favicon.ico",
+  organizationName: "Alishasami645",
   projectName: "Physical-AI-Humanoid-Robotics",
 
   presets: [
@@ -23,12 +86,6 @@ module.exports = {
       },
     ],
   ],
-
-  markdown: {
-    hooks: {
-      onBrokenMarkdownLinks: "warn",
-    },
-  },
 
   themeConfig: {
     navbar: {
